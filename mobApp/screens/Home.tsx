842cac7a280554fc1ee4a24baeb6c1fbf82b2327
Wilton Godinho <wgodinho@dynamite.ca>
2021-09-02 22:15:38 -0400

    in const

--- mobApp/screens/Home.tsx
+++ mobApp/screens/Home.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import api from '../api'
 
 const HomeScreen = ({ navigation }) => {
+  const [movies, setMovies] = useState([])
+
+  const fetchData = async () => {
+    try{
+      const resp = await api.getAllMovies()
+      const m = resp?.data?.data ?? []
+      setMovies(m)
+    }
+    catch (ex){
+      console.error(ex)
+    }
+}
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+  
     return (
           <View style={styles.container}>
-            <Text>Open up App.tsx to start working on your app!</Text>
+            <Text>{movies.length && movies[0] && movies[0].name} </Text>
             <Button title="Details" onPress={() => navigation.navigate('Details')} />
           </View>
     );
